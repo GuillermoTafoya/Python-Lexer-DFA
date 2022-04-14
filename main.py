a6eed2b58c3ae5f96f5eb31b7e312a64dd415975
Guillermo Tafoya <A01633790@tec.mx>
2022-04-13 19:37:29 -0500

    Final

--- main.py
+++ main.py
@@ -13,9 +13,35 @@ class DFA:
 
     def __str__(self):
         return "DFA: {}".format(self.transition_function)
+
     def lexerAritmetico(self, string:str)->list:
-        ### EDITAR ESTO ###
-        pass
+        tokens = []
+        pastStates = []
+        idx = 0
+        currentSubString = ""
+        self.current_state = self.transition_function[self.current_state][string[idx]]
+        while idx < len(string):
+
+            if string[idx] in self.transition_function[self.current_state]:
+                currentSubString += string[idx]
+                
+                self.current_state = self.transition_function[self.current_state][string[idx]]
+                idx += 1
+                pastStates.append(self.current_state)
+            elif string[idx] == " " or string[idx] == "":
+                currentSubString += string[idx]
+                idx += 1
+            else:
+                if currentSubString:
+                    tokens.append((self.current_state, currentSubString))
+                self.current_state = self.start_state
+                currentSubString = ""
+                if string[idx] == "\n":
+                    idx += 1
+        if currentSubString:
+                    tokens.append((self.current_state, currentSubString))
+        return tokens
+
 
     
 
@@ -44,56 +70,64 @@ if __name__ == '__main__':
 
             "Entero":{
                 ".":"Flotante", 
-                **{chr(num):"Entero" for num in range(48,58)},              # Numbers
-                "+": "Suma",
-                "-": "Resta",
-                "*": "Multiplicacion",
-                "/": "Division",
-                "^": "Potencia"
+                **{chr(num):"Entero" for num in range(48,58)},      # Numbers
 
             },
             "Flotante":{
-                **{chr(num):"Flotante" for num in range(48,58)},              # Numbers
-                "+": "Suma",
-                "-": "Resta",
-                "*": "Multiplicacion",
-                "/": "Division",
-                "^": "Potencia",
+                **{chr(num):"Flotante" for num in range(48,58)},    # Numbers
+                "E": "Exponente"
             }, 
-
-            "Asignación":None, 
-            "Suma":None, 
-            "Resta":None, 
-            "Multiplicación":None, 
-
-            "División":{
-                "/":"Comentario", 
-                **{chr(letter):"Variable" for letter in range(65,91)},      # Capital letters
-                **{chr(letter):"Variable" for letter in range(97,123)},     # Lowercase letters
-                **{chr(num):"Entero" for num in range(48,58)},              # Numbers
-                "+": "Suma",
-                "-": "Resta",
-                "*": "Multiplicacion",
-                "/": "Division",
-                "^": "Potencia",
-                "(": "Parentesis Izquierdo",
-                ")": "Parentesis Derecho",
-                "=": "Asignacion"
+            "Exponente":{
+                "-": "Flotante",
+                **{chr(num):"Flotante" for num in range(48,58)}     # Numbers
             },
 
-            "Potencia":None, 
-            "Variables":None, 
-            "Paréntesis Izquierdo":None, 
-            "Paréntesis Derecho":None,
-            "Comentario":None
+
+            # Operadores y parentesis
+            "Asignacion":dict(), 
+            "Suma":dict(), 
+            "Resta":dict(), 
+            "Multiplicacion":dict(), 
+            "Division":{
+                "/":"Comentario"
+            },
+            "Potencia":dict(),
+            "Parentesis Izquierdo":dict(), 
+            "Parentesis Derecho":dict(),
+            
+            "Variable":{
+                **{chr(letter):"Variable" for letter in range(65,91)},      # Capital letters
+                **{chr(letter):"Variable" for letter in range(97,123)},     # Lowercase letters
+                **{chr(num):"Variable" for num in range(48,58)},            # Numbers
+                "_": "Variable"
+
+            }, 
+            
+            "Comentario":{
+                **{chr(letter):"Comentario" for letter in range(65,91)},      # Capital letters
+                **{chr(letter):"Comentario" for letter in range(97,123)},     # Lowercase letters
+                **{chr(num):"Comentario" for num in range(48,58)},            # Numbers
+                "+": "Comentario",
+                "-": "Comentario",
+                "*": "Comentario",
+                "/": "Comentario",
+                "^": "Comentario",
+                "(": "Comentario",
+                ")": "Comentario",
+                "=": "Comentario"
+            },
+
+            "Fin de Estado":None
         },
+
         start_state="",
 
-        accept_states={"Entero","Flotante", "Asignación", "Suma", "Resta", "Multiplicación", 
-        "División", "Potencia", "Variables", "Paréntesis Izquierdo", "Paréntesis Derecho","Comentario"}
+        accept_states={"Fin de Estado"}
     )
-
-    from pprint import pprint
-    pprint(lexer.transition_function)
-    
+    with open("test.txt", "r") as file:
+        result = lexer.lexerAritmetico(file.read())
+    # Save the result in a text file as string pairs
+    with open("result.txt", "w") as file:
+        for token in result:
+            file.write("{},{}\n".format(token[0], token[1]))
 
